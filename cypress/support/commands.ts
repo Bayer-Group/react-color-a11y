147400255500cbe67e386dc0dfa8f86f9e5af2dc
David Lounsbrough <david.lounsbrough@gmail.com>
2024-01-19 12:15:52 -0600

    Fix type of Cypress helper

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,7 +2,7 @@
 
 import { colord } from 'colord'
 
-const expectColorsToMatch = (color1: string, color2: string) => {
+const expectColorsToMatch = (color1: string | undefined, color2: string | undefined) => {
   expect(Boolean(color1), 'expected both color to be defined or undefined').to.equal(Boolean(color2))
   expect(colord(color1).toHex()).to.be.equal(colord(color2).toHex())
 }
